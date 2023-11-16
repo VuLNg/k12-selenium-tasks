e27a58b378baa2b6c6193e79cc7efc45a2c300c0
Le Nguyen Vu <nguyenvu@Les-MacBook-Pro.local>
2023-11-16 13:35:16 +0700

    Lab-12 update

--- src/main/java/api_learning/InterviewQuestions.java
+++ src/main/java/api_learning/InterviewQuestions.java
@@ -1,46 +1,22 @@
 package api_learning;
 
+import dev.failsafe.internal.util.Assert;
 import driver.DriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class FormInteraction {
+import java.util.List;
+
+public class InterviewQuestions {
     private static final String TARGET_URL = "https://the-internet.herokuapp.com/login";
     private static final By USERNAME_SEL = By.id("username");
-    private static final By PASSWORD_SEL = By.cssSelector("#password");
-    private static final By LOGINBUTTON_SEL = By.cssSelector("#login [type='submit']");
-    private static final String USERNAME_STR = "tomsmith";
-    private static final String PASSWORD_STR = "SuperSecretPassword!";
     public static void main(String[] args) {
         WebDriver driver = DriverFactory.getWebDriver();
-        driver.get(TARGET_URL);
-        try{
-            WebElement usernameEle = driver.findElement(USERNAME_SEL);
-            WebElement passwordEle = driver.findElement(PASSWORD_SEL);
-
-            // Refresh page
-            driver.navigate().refresh();
-            usernameEle = driver.findElement(USERNAME_SEL);
-            passwordEle = driver.findElement(PASSWORD_SEL);
-            WebElement loginBtnEle = driver.findElement(LOGINBUTTON_SEL);
-            usernameEle.sendKeys(USERNAME_STR);
-            passwordEle.sendKeys(PASSWORD_STR);
-            loginBtnEle.click();
-
-            // Navigate back
-            driver.navigate().back();
-
-            // Debug purpose only
-            try{
-                Thread.sleep(2000);
-            } catch (Exception ignored){}
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
+        List<WebElement> usernameEle = driver.findElements(USERNAME_SEL);
+        if(usernameEle.isEmpty()) {
+            Assert.state(false,("The element is not found"));
         }
-
-        // Close the session | What if we have Exception
     }
 }
--- src/main/java/api_learning/LaunchBrowser.java
+++ src/main/java/api_learning/LaunchBrowser.java
@@ -1,2 +1,20 @@
-package api_learning;public class LaunchBrowser {
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getWebDriver();
+
+        // Open web page
+        driver.get("https://sdetpro.com");
+
+        // Close a window
+        driver.close();
+
+        // Quit the session
+        driver.quit();
+    }
 }
