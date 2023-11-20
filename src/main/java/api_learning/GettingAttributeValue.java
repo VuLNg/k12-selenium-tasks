35a1f7d27d108397c34a1a1907502f1cfb5435c3
Le Nguyen Vu <nguyenvu@Les-MacBook-Pro.local>
2023-11-20 13:53:07 +0700

    Day-13| Getting attribute value

---
+++ src/main/java/api_learning/GettingAttributeValue.java
@@ -0,0 +1,39 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class LinkTextInteraction {
+    private static final String TARGET_URL = "https://the-internet.herokuapp.com/login";
+
+
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getWebDriver();
+        driver.get(TARGET_URL);
+        try{
+/*           By powerByLinkTextSel = By.linkText("Elemental Selenium");
+           WebElement powerByLinkTextSelEle = driver.findElement(powerByLinkTextSel);
+           powerByLinkTextSelEle.click();*/
+
+            By powerByPartialLinkTextSel = By.partialLinkText("Elemental");
+            WebElement powerByPartialLinkTextSelEle = driver.findElement(powerByPartialLinkTextSel);
+            powerByPartialLinkTextSelEle.click();
+
+            // Debug purpose only
+            try {
+                Thread.sleep(200);
+            } catch (Exception ignored) {}
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+
+    }
+}
